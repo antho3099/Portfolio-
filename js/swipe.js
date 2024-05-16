103408beb1c13aca1c35320ec3e7600c309f6df4
ANG <antho.nguyen30@gmail.com>
2024-05-16 12:47:58 +0200

    Portfolio

--- js/swipe.js
+++ js/swipe.js
@@ -1,5 +1,5 @@
-let swipe = document.querySelectorAll("#projets .swipe");
-let diapo = document.querySelectorAll("#projets .diapo");
+let swipe = document.querySelectorAll(".swipe");
+let diapo = document.querySelectorAll(".diapo");
 diapo.addEventListener('scroll', function(){
     if("scroll"){
        swipe.style.display ="none"; 
